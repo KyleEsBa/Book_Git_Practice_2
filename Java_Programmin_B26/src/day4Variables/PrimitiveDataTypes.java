1d25f7de05b3e47f21cd7188f31d3ffda0fe4652
KyleEsBa <kyleb.-95@hotmail.com>
2022-02-05 15:46:06 -0500

    Prueba de Commit

---
+++ Java_Programmin_B26/src/day4Variables/PrimitiveDataTypes.java
@@ -0,0 +1,11 @@
+package day4Variables;
+
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        byte age=60;
+        System.out.println(age);
+
+        short year;
+        year=2022;
+    }
+}
