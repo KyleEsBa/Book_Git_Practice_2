8cb2b3f6cadf6c3add9a46f588465089fd17ab43
KyleEsBa <kyleb.-95@hotmail.com>
2022-02-08 20:56:32 -0500

    UnaryOperators.1

---
+++ Java_Programmin_B26/src/day_07_unary_operators/IncrementExample.java
@@ -0,0 +1,23 @@
+package day_07_unary_operators;
+
+public class IncrementExample {
+    public static void main(String[] args) {
+        int x=0;
+        System.out.println(x);
+
+        x=x+1;
+        System.out.println(x);
+
+        x++;
+        System.out.println(x);
+
+        ++x;
+        System.out.println(x);
+
+        /*Diference between x++ and ++x
+        * ++x: pre increment
+        * x++: post increment*/
+        System.out.println(++x);//First add the value after that print it
+        System.out.println(x++);//First print the actual value after that increment the value
+    }
+}
