72d4ac029a143731c42c46f82ee433d2115fdad8
KyleEsBa <kyleb.-95@hotmail.com>
2022-02-09 22:15:52 -0500

    Precedence operators: Priority operators

---
+++ Java_Programmin_B26/src/day8_relational_operators/AllOperators.java
@@ -0,0 +1,26 @@
+package day8_relational_operators;
+
+public class AllOperators {
+    public static void main(String[] args) {
+        System.out.println(5 + 2 * 3);
+        //Priority 1:   (2 * 3)
+        //Priority 2:   5 + (6)
+        //Result:       11
+
+        System.out.println(5 * 2 + 4 / 2);
+        //Priority 1:   (5 * 2) and (4 / 2)
+        //Priority 2:   (10 + 2)
+        //Result:       12
+
+        System.out.println(5 + 2 > 4 - 3);
+        //Priority 1:   (5 + 2) and (4 - 3)
+        //Priority 2:   (7 > 1)
+        //Result:       true
+
+
+        int a = 20;//20
+        int b= -a--     +   a++     +   --a     *     a--;
+        //     -20      +   19      +   19      *     19
+        System.out.println(b);
+    }
+}
