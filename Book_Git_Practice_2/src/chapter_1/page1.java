d63b36a9a631932908372fffbcdf7c26f5a69de0
KyleEsBa <kyleb.-95@hotmail.com>
2022-04-26 11:39:40 -0500

    Commit of the first two pages

---
+++ Book_Git_Practice_2/src/chapter_1/page1.java
@@ -0,0 +1,5 @@
+package chapter_1;
+
+public class page1 {
+    //This is the first line Kyle writes
+}
---
+++ Book_Git_Practice_2/src/chapter_1/page2.java
@@ -0,0 +1,5 @@
+package chapter_1;
+
+public class page2 {
+    //text for review in git repo
+}
